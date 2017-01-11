6a80c03234a3b3ba7ad79d450a0601e6ef6df5aa
MixMasterT <torahlight@gmail.com>
2017-01-10 17:33:41 -0800

    Implement basic login functionality from the frontend, current access only through store placed directly on window for testing purposes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  validates :username, :password_digest, :session_token, null: false
-  validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :username, :email, :password_digest, :session_token, null: false
+  validates :password, length: { minimum: 6, allow_nil: true }
 
   before_validation :ensure_token
 
@@ -8,17 +8,17 @@ class User < ActiveRecord::Base
 
   def self.check_cred(username, password)
     u = User.find_by(username: username)
+    return nil unless u
     u.is_password?(password) ? u : nil
   end
 
   def password=(word)
     @password = word
     self.password_digest = BCrypt::Password.create(word)
-    self.save
   end
 
   def is_password?(word)
-    BCrypt::Password.new(self.pass_hash).is_password?(word)
+    BCrypt::Password.new(self.password_digest).is_password?(word)
   end
 
   def reset_token!
---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,20 @@
+export const signup = (user) => (
+  $.ajax({
+    url: '/api/users',
+    method: 'POST',
+    data: { user }
+  })
+);
+export const login = (cred) => (
+  $.ajax({
+    url: '/api/session',
+    method: 'POST',
+    data: { cred }
+  })
+);
+export const logout = () => (
+  $.ajax({
+    url: '/api/session',
+    method: 'DELETE'
+  })
+);
