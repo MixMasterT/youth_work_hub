13daf691f3334ba2944a0757e51f4fa38f599cc7
MixMasterT <torahlight@gmail.com>
2017-01-15 22:16:18 -0800

    Fix WorkersReducer to consistently treat workers as an array

--- frontend/reducers/workers_reducer.js
+++ frontend/reducers/workers_reducer.js
@@ -5,13 +5,12 @@ import merge from 'lodash/merge';
 
 const WorkersReducer = (state = [], action) => {
   Object.freeze(state);
-  let newState = merge({}, state);
   switch(action.type) {
     case RECEIVE_ALL_WORKERS:
       return action.workers;
 
     case RECEIVE_CURRENT_WORKER:
-      return merge(newState, action.worker);
+      return [...state, action.worker];
 
     default:
       return state;
