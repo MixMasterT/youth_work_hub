757cd54654daee8e8e9f1102de2b3f4dc978d252
App Academy Student <student@appacademy.io>
2017-01-16 21:37:04 -0800

    Improve seed data to include five worker records with pictures.

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,34 @@ Worker.create(username:"Timmy",
             email:"timmy@oldtimey.com",
             bio:"I love riding my bike, so make a good delivery boy. I have worked delivering newspapers and groceries. If you need anything delivered, I'm your man. I can also do yard work. I hope that I can save up enough money to buy a new bicycle soon!",
             birth_date:"11/21/2004",
-            picture_url: "http://res.cloudinary.com/youth-work-hub/image/upload/v1484531943/ihtivxsbxovc8xo6xfva.png");
+            zip_code:"94314",
+            picture_url: "http://res.cloudinary.com/youth-work-hub/image/upload/v1484624792/almzrlg2cjyapu3zocss.png");
+
+Worker.create(username:"Arnold",
+            password:"password",
+            email:"arnie@yahoo.com",
+            bio:"I love skateboarding. I will entertain you with my excellent skateboarding skills. Hire me as a pro rider!",
+            birth_date:"11/21/2004",
+            zip_code:"94235",
+            picture_url: "https://res.cloudinary.com/youth-work-hub/image/upload/v1484626488/dfw4azklc4beac7ecqkt.png");
+
+Worker.create(username:"Sam",
+            password:"password",
+            email:"sammie@computerlovers.com",
+            bio:"Ever since I was eight years old I have been called a 'genius' with computers. I am actually pretty good at setting up websites, databases, and whatever else you might need. I will solve your computer problems!",
+            birth_date:"10/18/2002",
+            min_wage: "20",
+            zip_code: "95323",
+            picture_url: "http://res.cloudinary.com/youth-work-hub/image/upload/v1484628047/mszesro3mz8nixpw5uer.png");
+
+Worker.create(username:"Sarah",
+            password:"password",
+            email:"sarah@babysitters.com",
+            bio:"I love reading books, and I am really looking forward to going to college. However, my family doesn't have a lot of money, so I am trying to earn money now that I can save to help me get through college. I am good with kids, so I can do babysitting or any other household work you might need.",
+            birth_date:"5/28/2004",
+            zip_code:"97212",
+            min_wage: "12",
+            picture_url: "http://res.cloudinary.com/youth-work-hub/image/upload/v1484588140/txto2vhpouerfnfo52uh.png");
 
 five_users = (1..5).to_a
 five_users.each do |n|
@@ -28,5 +55,8 @@ end
 
 five_workers = (1..5).to_a
 five_workers.each do |n|
-  Worker.create(username: "W#{n}", password:"#{n.to_s * 6}", email:"W#{n}@w.com")
+  Worker.create(username: "W#{n}",
+                  password:"#{n.to_s * 6}",
+                  email:"W#{n}@w.com",
+                  zip_code: "94131")
 end
