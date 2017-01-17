d4430c0d255a19783649bd9acbc1d22217f5aec5
MixMasterT <torahlight@gmail.com>
2017-01-16 19:09:51 -0800

    add some details to Worker Show View

--- frontend/components/worker_show/worker_show.jsx
+++ frontend/components/worker_show/worker_show.jsx
@@ -1,51 +1,38 @@
 import React from 'react';
 
-// class WorkerShow extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.fetchWorkers = this.fetchWorkers.bind(this);
-//   }
-//
-//   fetchWorkers() {
-//     this.props.getWorkers();
-//   }
-//
-//   redirectTo(str) {
-//     return () => {
-//       hashHistory.push(`/workers/${str}`);
-//     };
-//   }
-//
-//   render() {
-//     const workers = this.props.workers;
-//     let workerIds = Object.keys(workers);
-//     console.log(workerIds);
-//
-//     const  workersArray = workerIds.map((id) => (
-//       <WorkerShowItem key={id}
-//                         worker={workers[id]}
-//                         onClick={this.redirectTo(id)}/>
-//     ));
-//
-//     return (
-//       <div className="workers-index">
-//         <h1>We offer the best young workers around!</h1>
-//         { workersArray }
-//       </div>
-//     );
-//   }
-// }
-
 const WorkerShow = ({worker, getWorkers}) => {
   console.log(worker);
   if (worker) {
     return (
-      <h2>This is a Worker Show page for {worker.username}</h2>
+      <div className='worker-show'>
+        <h2>Worker Profile</h2>
+        <img src={worker.picture_url} />
+        <div className='worker-show-text'>
+          <h4>{worker.username}</h4>
+          <label>bio:
+            <p>{(worker.bio) ? worker.bio :
+                  "This worker has not provided a biography."}
+            </p>
+          </label>
+          <label>birth_date:
+            <p>{(worker.birth_date) ? worker.birth_date :
+                  "This worker's date is not in our records."}</p>
+          </label>
+          <label>zip_code:
+            <p>{(worker.zip_code) ? worker.zip_code :
+                  "This worker's zipcode is not available'."}</p>
+          </label>
+          <label>minimum desired wage:
+            <p>{(worker.min_wage) ? worker.min_wage :
+                  "This worker has not chosen a minimum wage."}</p>
+          </label>
+        </div>
+      </div>
     );
   } else {
     getWorkers();
     return (
-      <h3>Information on the worker you seek is currently unavailable.</h3>
+      <h2>Information about the worker you seek is not available.</h2>
     );
   }
 };
