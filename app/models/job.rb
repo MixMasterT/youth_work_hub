527cf285f5a094018b20e5e6ff27d906075058d6
MixMasterT <torahlight@gmail.com>
2017-01-17 11:46:11 -0800

    Fix Job model validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,16 +2,17 @@ class Job < ActiveRecord::Base
   validates :user_id,
             :description,
             :job_type,
-            :session_token,
-            presence: true
+            :wage,
+            :start_time,
+            :cost, presence: true
 
   validate :has_address_or_gps
 
   belongs_to :user
 
-  def has_zip_or_gps
+  def has_address_or_gps
     if address.blank? && (lat.blank? || lng.blank?)
-      errors.add(:base, "Please provide either gps coords or zipcode")
+      errors.add(:base, "Please provide either gps coords or an address for this job.")
     end
   end
 end
