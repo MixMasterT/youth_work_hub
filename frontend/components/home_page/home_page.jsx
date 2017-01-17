ce825c55b809cc422ec3b8cf8f656fd76406a6ef
MixMasterT <torahlight@gmail.com>
2017-01-17 11:20:16 -0800

    Improve styling of homepage and add content to sample testimonials, also add styles to login-box

--- frontend/components/home_page/home_page.jsx
+++ frontend/components/home_page/home_page.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import classnames from 'classnames';
+
 const MissionStatement = () => (
   <div className="mission-statement">
     <h2>Become an Employer</h2>
-    <h3>Help young people learn responsibility and get your jobs done in
-      the process</h3>
+    <p>At Youth Work Hub you can be an employer. Our standard users
+      understand the benefits of learning financial responsibility
+      from a young age. Help young people in your community earn, learn,
+      and grow... and get help with odd jobs that you need to have finished.
+      </p>
+    <h3>Help young people learn and get your jobs done!</h3>
     <p>
       Youth Work Hub is a non-profit organization dedicated to enhancing
       communities by hooking young people up with jobs that need to be
@@ -26,27 +32,34 @@ const MissionStatement = () => (
 
 const Testimonials = () => {
   const testimons = [
-    {title: "Great Experience",
-      text: "WTF I put a string in here and it turned RED!",
-      picture_url: "balbalbal"},
+    {title: "My Summer Was Way Better Because of Youth Work Hub!",
+      text: "Last summer was boring for me. My parents work, so I had to spend most of my time alone at home. This summer, I found a job through Youth Work Hub. Working helped me learn a lot, meet some very interesting people, and earn money. I am excited because I was able to buy my own cell phone. Thanks to the people at Youth Work Hub for helping me have  a great summer!",
+      user: "Liana",
+      picture_url:
+      "http://res.cloudinary.com/youth-work-hub/image/upload/v1484521265/4.28.16-5-Reasons-Youth-Volunteers-Make-Better-Hires_st8ul5.png"
+    },
 
-    {title: "Great Experience",
-      text: "WTF I put a string in here and it turned RED!",
-      picture_url: "balbalbal"},
+    {title: "We Got Much Needed Help",
+      user: "Debrah Rennolds",
+      text: "Our Campaign was having a lot of trouble reaching diverse members of our community. We found some great workers to help use bridge the gap on Youth Work Hub. We have been very impressed with their work ethic. These bright young men and women represent our future!",
+      picture_url: "http://res.cloudinary.com/youth-work-hub/image/upload/v1484287815/YouthBuild-Guest-post-1_vaby6h.jpg"
+    },
 
-    {title: "Great Experience",
-      text: "WTF I put a string in here and it turned RED!",
-      picture_url: "balbalbal"}
+    {title: "Saving Money For College",
+      text: "I know college is very expensive. My mom has reminded me a lot that it will cost a lot of money, and that I should try not to get into debt. She is still paying of her college loans! So, for the past two summers I have been earning money mowing lawns. Youth Work Hub has made it easy to find clients!",
+      user: "Martin",
+      picture_url: "http://res.cloudinary.com/youth-work-hub/image/upload/v1484288385/lawnmower1_ip9bas.jpg"
+    }
   ];
+  const classes = classnames({'testimonial': true, 'clearfix': true});
   return (
     <div className="testimonials">
       {testimons.map((tes, id) => (
-        <div className="testmimonial"
-             key={id}
-        >
+        <div className={classes} key={id}>
           <h4>{tes.title}</h4>
-          <img  />
+          <img src={tes.picture_url} />
           <p>{tes.text}</p>
+          <p className="attribution">-{tes.user}</p>
         </div>
       ))}
     </div>
@@ -56,7 +69,7 @@ const Testimonials = () => {
 const HomePage = () => (
   <div className="home">
     <h1>Build Your Community</h1>
-    <div>
+    <div className="home-body">
       <MissionStatement />
       <Testimonials />
     </div>
