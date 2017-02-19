fcd812658201f2416478091e3f45db468b8e4d39
mixmastert <torahlight@gmail.com>
2017-02-19 01:55:13 -0800

    Add geolocation call to JobsMap, map now recenters and sets pin for user location

--- frontend/components/maps/jobs_map.jsx
+++ frontend/components/maps/jobs_map.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+
+import MarkerManager from '../../util/marker_manager';
 
 const getCoordsObj = latLng => ({
   lat: latLng.lat(),
@@ -16,23 +18,33 @@ class JobsMap extends React.Component {
   }
 
   componentDidMount() {
-    const mapOptions = {
+    let mapOptions = {
       center: { lat: 37.7758, lng: -122.435 },
       zoom: 13
     }
+
+    navigator.geolocation.getCurrentPosition((loc) => {
+      if (loc.coords.latitude) {
+        this.map.setCenter(new google.maps.LatLng(loc.coords.latitude,
+                                                loc.coords.longitude));
+        this.addMarker(loc.coords.latitude, loc.coords.longitude);
+      }
+    });
+
 
     this.map = new google.maps.Map(this.mapNode, mapOptions);
 
     google.maps.event.addListener(this.map, 'click', event => {
       this.addMarker(event.latLng);
     });
+
+    this.markerManager = new MarkerManager(this.map);
   }
 
   addMarker(lat, lng) {
-
     if (this.state.marker) { this.state.marker.setMap(null); }
     const marker = new google.maps.Marker({
-      position: coords,
+      position: { lat, lng },
       map: this.map,
       title: this.props.markerTitle
     })
---
+++ frontend/util/marker_manager.js
@@ -0,0 +1,14 @@
+export default class MarkerManager {
+  constructor(map) {
+    this.map = map;
+    this.markers = [];
+  }
+
+  addUserLocation(location) {
+
+  }
+
+  addJobLocations(jobs) {
+
+  }
+}
